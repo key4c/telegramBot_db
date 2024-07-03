e80401e2c4f6721991c2b6cc130027f923688317
CitizenKey <key4citizen@gmail.com>
2024-07-03 20:53:49 +0300

    added .env

--- 4_SQLite.py
+++ 4_SQLite.py
@@ -1,22 +1,41 @@
-import telebot
-import sqlite3
+import os
+import telebot  # Импортируем библиотеку для работы с Telegram Bot API
+import sqlite3  # Импортируем библиотеку для работы с SQLite
+from dotenv import load_dotenv
 
-bot = telebot.TeleBot('7352742662:AAHy0KFOrjt356Tt1024MywDE25b97vkQAk')
+# Загрузка переменных окружения из .env файла
+load_dotenv()
 
+# Получение токена из переменной окружения
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+
+bot = telebot.TeleBot(TELEGRAM_TOKEN)
+
+# Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
-    conn = sqlite3.connect('citizen.db')  # Исправлено на sqlite3.connect
-    cur = conn.cursor()
+    # Подключаемся к базе данных SQLite (если файл базы данных не существует, он будет создан)
+    conn = sqlite3.connect('citizen.db')  # sqlite3.connect используется для подключения к базе данных
+    cur = conn.cursor()  # Создаем курсор для выполнения SQL-запросов
 
-    cur.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, pass TEXT)')
-    conn.commit()
-    cur.close()
-    conn.close()
+    # Создаем таблицу users, если она еще не существует
+    cur.execute('''CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT,
+                    pass TEXT
+                )''')
+    conn.commit()  # Подтверждаем изменения в базе данных
+    cur.close()  # Закрываем курсор
+    conn.close()  # Закрываем соединение с базой данных
 
+    # Отправляем приветственное сообщение пользователю
     bot.send_message(message.chat.id, 'Привет, сейчас тебя зарегистрируем! Введи свое имя.')
+    # Регистрируем следующий шаг для обработки имени пользователя
     bot.register_next_step_handler(message, user_name)
 
+# Функция для обработки имени пользователя
 def user_name(message):
-    pass
+    pass  # Заглушка, здесь будет логика для обработки имени пользователя
 
+# Запускаем бот в режиме непрерывного опроса
 bot.polling(non_stop=True)
